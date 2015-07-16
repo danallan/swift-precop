74010ab561ea3a85c5d849ab9f278bd692069f31
Dan Armendariz <danallan@cs.harvard.edu>
2015-07-16 15:38:12 -0400

    eliot bubble sort skeleton code

---
+++ day-3/eliot-bubble.playground/Contents.swift
@@ -0,0 +1,44 @@
+/*
+ * YOUR NAME HERE
+ * you@email.com
+ *
+ * Bubble sort
+
+To understand Bubble sort, first watch this video:
+https://www.youtube.com/watch?v=8Kp-8OGwphY
+
+You might also use this tool to help visualize the sort:
+https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html
+Select "Bubble sort", and compare it against others if you want!
+
+If you did compare other sorts, you might realize that Bubble sort isn't the most efficient way to sort a list. However, it is one of the easiest to reason about and so it is a good sort to implement first.
+
+Try to implement it below. We have provided some skeleton code to get you started. Be sure to complete all the "TODO"s!
+ */
+
+let nums = [8, 2, 10, 9, 11, 1, 7, 3, 4]
+
+func bubbleSort(var list: [Int]) -> [Int]
+{
+    for i in 0 ..< /* TODO */
+    {
+        for j in 0 ..< /* TODO */
+        {
+            if (/* TODO */)
+            {
+                /* TODO */
+            }
+        }
+    }
+    return list
+}
+
+// Here are some test cases
+bubbleSort([1])
+bubbleSort([5, 3])
+bubbleSort([1, 2])
+bubbleSort([-42, 0, 12, 6, -5])
+bubbleSort([8, 2, 10 , 9, 11, 1, 7, 3, 4])
+bubbleSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+
+/* TODO: write more test cases */
